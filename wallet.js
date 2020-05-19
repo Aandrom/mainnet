80bf22d779a008d096aaa03ec2c3f837bbbf7bb0
Aandrom <65248669+Aandrom@users.noreply.github.com>
2020-05-19 18:40:29 +0200

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -451,7 +451,7 @@ window.addEventListener('load', function () {
     userbalance()
     setInterval(function () {
       userbalance();
-      canActiveZIRr();
+      canActivateZIRr();
     },10000);
 
 
